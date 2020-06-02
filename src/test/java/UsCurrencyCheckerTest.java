a011252a8da4671a91437fc524c57d549a33997a
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-06-02 11:54:12 +0200

    Changed test names and split one test

--- src/test/java/UsCurrencyCheckerTest.java
+++ src/test/java/UsCurrencyCheckerTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UsCurrencyCheckerTest {
     @Test
-    public void testValidCurrencyString() {
+    public void itShouldMatchAValidCurrencyString() {
         checkRegex("$ 149.50");
     }
 
@@ -22,49 +22,54 @@ public class UsCurrencyCheckerTest {
     }
 
     @Test
-    public void testNoCurrency() {
+    public void itShouldNotMatchAStringWithouCurrency() {
         Matcher matcher = UsCurrencyChecker.getMatcher("This is text without any currency information");
         assertFalse(matcher.find());
     }
 
     @Test
-    public void testNoBlankNoZero() {
+    public void itShouldMatchACurrencyWithNoBlankBetweenDollarAndFirstDigit() {
+        checkRegex("$149.50");
+    }
+
+    @Test
+    public void itShouldMatchACurrencyWithAMissingZeroAtEnd() {
         checkRegex("$149.5");
     }
 
     @Test
-    public void testOnlyBlankBetween$AndNumber() {
+    public void itShouldNotMatchADifferentCharThanBlankBetweenDollarAndFirstDigit() {
         Matcher matcher = UsCurrencyChecker.getMatcher("$x14.5");
         assertFalse(matcher.find());
     }
 
     @Test
-    public void testNoDecimals() {
+    public void itShouldMatchANumberWithNoDecimalPoint() {
         checkRegex("$ 200");
     }
 
     @Test
-    public void testCurrencyEmbeddedInAText() {
+    public void itShouldMatchACurrencyEmbeddedInAText() {
         checkRegex("This makes $ 149.30 in total", "$ 149.30");
     }
 
     @Test
-    public void testMoreThanTwoDecimals() {
+    public void itShouldMatchACurrencyWithMoreThanTwoDecimals() {
         checkRegex("$1.214", "$1.21");
     }
 
     @Test
-    public void testShortestCurrencyPossible() {
+    public void itShouldMatchTheShortestCurrencyPossible() {
         checkRegex("$3");
     }
 
     @Test
-    public void testDecimalPointButNoDecimals() {
+    public void itShouldMatchACurrencyWithADecimalPointButNoDecimals() {
         checkRegex("$ 3468.", "$ 3468");
     }
 
     @Test
-    public void testCommaInsteadOfPoint() {
+    public void itShouldNotMatchACommaInsteadOfADecimalPoint() {
         checkRegex("$ 3,14", "$ 3");
     }
 }
